f9fd6fcd51dd1c880ebcbfcc86bb0be552beb9c8
lanliwz <lanliwz@yahoo.com>
2015-03-05 21:30:28 -0500

    inprogress

--- FastPricingEngine/src/com/upupconsultant/pricing/test/TestMe.java
+++ FastPricingEngine/src/com/upupconsultant/pricing/test/TestMe.java
@@ -2,15 +2,14 @@ package com.upupconsultant.pricing.test;
 
 import org.drools.builder.KnowledgeBuilder;
 import org.drools.builder.KnowledgeBuilderFactory;
-import org.drools.io.ResourceFactory;
-import org.drools.builder.ResourceType;
+
+
 
 
 public class TestMe {
 
 
 	
-	KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-	//kbuilder.add(ResourceFactory.newUrlResource("file://myrules.drl"),ResourceType.DRL);
-	//KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+	KnowledgeBuilder builder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+	//builder.add(ResourceFactory.newByteArrayResource(drl.getBytes()), ResourceType.DRL);
 }
