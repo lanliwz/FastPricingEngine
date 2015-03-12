864ad121de5482369d49d8195e778d23b89d63f3
lanliwz <lanliwz@yahoo.com>
2015-03-11 21:08:05 -0400

    add benefit calc

---
+++ FastPricingEngine/src/com/upupconsultant/pricing/process/BenefitCalculator.java
@@ -0,0 +1,41 @@
+package com.upupconsultant.pricing.process;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BenefitCalculator {
+	private String groupName;
+	private Map<String,Double> rateSheet;
+	public BenefitCalculator(String groupName,Map<String,Double> rateSheet){
+		this.groupName=groupName;
+		this.rateSheet=rateSheet;
+	}
+	public double getTotalAmout(double amount){
+		Map<String,Double> items = getItemAmount(amount);
+		double total=0;
+		for(double amt:items.values()){
+			total += amt;
+		}
+		
+		return total;
+	}
+	public Map<String,Double> getItemAmount(double amount){
+		Map<String,Double> items = new HashMap<String,Double>();
+		
+		for(String key:rateSheet.keySet()){
+			String[] category = key.split(",");
+			double itemAmt=0;
+			if (category[1].equals("P")){
+				itemAmt=amount*rateSheet.get(key);
+			}else if (category[1].equals("C")){
+				itemAmt=rateSheet.get(key);
+			}
+			items.put(category[0], itemAmt);
+			
+			
+		}
+		return items;
+		
+	}
+
+}
--- FastPricingEngine/src/com/upupconsultant/pricing/test/TestClaims.java
+++ FastPricingEngine/src/com/upupconsultant/pricing/test/TestClaims.java
@@ -1,11 +1,15 @@
 package com.upupconsultant.pricing.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import com.upupconsultant.pricing.event.PricingEvent;
 import com.upupconsultant.pricing.io.Dao;
 import com.upupconsultant.pricing.io.FileDao;
 import com.upupconsultant.pricing.model.PricingEntity;
+import com.upupconsultant.pricing.process.BenefitCalculator;
 import com.upupconsultant.pricing.process.CEProcessor;
 import com.upupconsultant.pricing.rule.RuleManager;
 import com.upupconsultant.pricing.service.PricingService;
@@ -23,6 +27,15 @@ public class TestClaims {
 			rmgr.setDao(new FileDao());
 			rmgr.setRuleFileRoot("/Users/lanliwz/FastPricingEngine/rule");
 			rmgr.setRuleFileBackupRoot("/Users/lanliwz/FastPricingEngine/ruleBackup");
+			
+		Map<String,Double> hmoCalc = new HashMap<String,Double>();
+		hmoCalc.put("BASE,P", 0.9);
+		hmoCalc.put("COPAY1,P",-0.01);
+		hmoCalc.put("COPAY2,P", -0.005);
+		
+		BenefitCalculator hmocalc = new BenefitCalculator("HMO",hmoCalc);
+		
+		System.out.println(hmocalc.getTotalAmout(1000));
 		
 		CEProcessor cep = new CEProcessor(rmgr);
 		Dao dao = new FileDao();
